e2fb70f501b88a7e3011e4799cf4e8a9cb4a8e36
jknow <joeko1337@gmail.com>
2025-12-17 16:56:43 -0500

    Add BFS, DFS, and Topological sort

--- src/main/java/org/example/Graph.java
+++ src/main/java/org/example/Graph.java
@@ -1,9 +1,6 @@
 package org.example;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class Graph {
     private final Map<Integer, List<Integer>> adjacencyList;
@@ -44,5 +41,79 @@ public class Graph {
             }
             System.out.println();
         }
+    }
+
+    public boolean bfsSearch(int source, int destination) {
+        Queue<Integer> q = new LinkedList<>();
+        Set<Integer> visited = new HashSet<>();
+        visited.add(source);
+        q.add(source);
+
+        while (!q.isEmpty()) {
+            int vertex = q.poll();
+            for (Integer neighbor : this.adjacencyList.get(vertex)) {
+                if (neighbor == destination) {
+                    return true;
+                }
+                if (!visited.contains(neighbor)) {
+                    visited.add(neighbor);
+                    q.add(neighbor);
+                }
+            }
+        }
+        return false;
+    }
+
+    public boolean dfsSearch(int source, int destination) {
+        Stack<Integer> stack = new Stack<>();
+        Set<Integer> visited = new HashSet<>();
+        visited.add(source);
+        stack.push(source);
+
+        while (!stack.isEmpty()) {
+            int vertex = stack.pop();
+            for (Integer neighbor : this.adjacencyList.get(vertex)) {
+                if (neighbor == destination) {
+                    return true;
+                }
+                if (!visited.contains(neighbor)) {
+                    visited.add(neighbor);
+                    stack.push(neighbor);
+                }
+            }
+        }
+        return false;
+    }
+
+    public List<Integer> topologicalSort() {
+        Stack<Integer> stack = new Stack<>();
+        Set<Integer> visited = new HashSet<>();
+
+        for (Map.Entry<Integer, List<Integer>> vertex : this.adjacencyList.entrySet()) {
+            if (!visited.contains(vertex.getKey())) {
+                topologicalDfs(vertex.getKey(), vertex.getValue(), stack, visited);
+            }
+        }
+
+        List<Integer> output = new ArrayList<>(stack.size());
+        while (!stack.isEmpty()) {
+            output.add(stack.pop());
+        }
+        return output;
+    }
+
+    private void topologicalDfs(
+            Integer vertex,
+            List<Integer> neighbors,
+            Stack<Integer> stack,
+            Set<Integer> visited
+    ) {
+        visited.add(vertex);
+        for (Integer neighbor : neighbors) {
+            if (!visited.contains(neighbor)) {
+                topologicalDfs(neighbor, this.adjacencyList.get(neighbor), stack, visited);
+            }
+        }
+        stack.push(vertex);
     }
 }
