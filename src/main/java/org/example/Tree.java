4448deaebad1c715152cb47797e762435d37acf0
jknow <joeko1337@gmail.com>
2026-01-13 14:11:11 -0500

    Add functions for tree traversals

--- src/main/java/org/example/Tree.java
+++ src/main/java/org/example/Tree.java
@@ -1,5 +1,6 @@
 package org.example;
 
+import java.util.Deque;
 import java.util.LinkedList;
 import java.util.Queue;
 
@@ -61,6 +62,103 @@ public class Tree {
 
         // Print left subtree
         printTree(node.left, depth + 1);
+    }
+
+    public void preorderRecursive() {
+        preorderRecursive(this.root);
+    }
+
+    private static void preorderRecursive(TreeNode root) {
+        if (root == null) return;
+        System.out.println(root.val);
+        preorderRecursive(root.left);
+        preorderRecursive(root.right);
+    }
+
+    public void inorderRecursive() {
+        inorderRecursive(this.root);
+    }
+
+    private static void inorderRecursive(TreeNode root) {
+        if (root == null) return;
+        inorderRecursive(root.left);
+        System.out.println(root.val);
+        inorderRecursive(root.right);
+    }
+
+    public void postorderRecursive() {
+        postorderRecursive(this.root);
+    }
+
+    private static void postorderRecursive(TreeNode root) {
+        if (root == null) return;
+        postorderRecursive(root.left);
+        postorderRecursive(root.right);
+        System.out.println(root.val);
+    }
+
+    public void preorderIterative() {
+        preorderIterative(this.root);
+    }
+
+    private static void preorderIterative(TreeNode root) {
+        Deque<TreeNode> stack = new LinkedList<>();
+        stack.push(root);
+
+        while (!stack.isEmpty()) {
+            TreeNode curr = stack.pop();
+            System.out.println(curr.val);
+            if (curr.right != null) stack.push(curr.right);
+            if (curr.left != null) stack.push(curr.left);
+        }
+    }
+
+    public void inorderIterative() {
+        inorderIterative(this.root);
+    }
+
+    private static void inorderIterative(TreeNode root) {
+        Deque<TreeNode> stack = new LinkedList<>();
+        TreeNode curr = root;
+
+        while (!stack.isEmpty() || curr != null) {
+            while (curr != null) {
+                stack.push(curr);
+                curr = curr.left;
+            }
+
+            curr = stack.pop();
+            System.out.println(curr.val);
+
+            curr = curr.right;
+        }
+    }
+
+    public void postorderIterative() {
+        postorderIterative(this.root);
+    }
+
+    private static void postorderIterative(TreeNode root) {
+        Deque<TreeNode> stack = new LinkedList<>();
+        TreeNode curr = root;
+        TreeNode prev = null;
+
+        while (!stack.isEmpty() || curr != null) {
+            while (curr != null) {
+                stack.push(curr);
+                curr = curr.left;
+            }
+
+            curr = stack.element();
+            if (curr.right != null && curr.right != prev) {
+                curr = curr.right;
+            } else {
+                curr = stack.pop();
+                System.out.println(curr.val);
+                prev = curr;
+                curr = null;
+            }
+        }
     }
 
 }
